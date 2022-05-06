cec789f42c18c8e10760525b9f9a4b9495812362
luizgasp <luizegasp@gmail.com>
2022-05-06 12:08:24 -0300

    change-android-rules

--- lib/app/modules/domain/repositories/objects_repositories.dart
+++ lib/app/modules/domain/repositories/objects_repositories.dart
@@ -1,6 +1,6 @@
 import 'package:measurement/app/modules/domain/entities/object_entity.dart';
 
 abstract class ObjectsRepository {
-  Future<void> create(double width, double height);
+  Future<void> create(String urlImage);
   Future<List<ObjectEntity>> list();
 }
